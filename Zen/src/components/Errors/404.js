75e5d22c8f4b04d71dcf0b01f218b320801d8997
Rodrigo Mungo <rodrigomungo@gmail.com>
2019-06-07 05:48:33 -0300

    Se agrega componente para páginas no encontradas con código 404.

---
+++ Zen/src/components/Errors/404.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Page404 extends Component {
+
+    render () {
+      return (
+        <div className="Content">
+            <h1>404 - Página no encontrada</h1>
+        </div>
+      );
+    }
+}
+
+export default Page404;
